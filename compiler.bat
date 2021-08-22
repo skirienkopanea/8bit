567152af9ad9e82c05a2af32358e4d5eeb6f2073
Sergio Kirienko Panea <s.kirienkopanea@student.tudelft.nl>
2021-08-22 11:21:03 +0200

    hmmm

---
+++ compiler.bat
@@ -0,0 +1,5 @@
+cd out/production/8bit
+java Main "C:/Users/sergio/OneDrive/Desktop/8bit/input" "C:/Users/sergio/OneDrive/Desktop/8bit/output"
+
+python "C:/Users/sergio/OneDrive/Desktop/Computer-Simulation/cpu.py" output.txt
+pause
